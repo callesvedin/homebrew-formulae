70bbe5216aceaaf1a1e3b6e65926cad61f9c8b59
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2021-04-08 14:50:48 +0200

    Changed xcode requirement to xcode 11, tagged 0.1.1

--- Formula/templaterunner.rb
+++ Formula/templaterunner.rb
@@ -2,7 +2,7 @@ class Templaterunner < Formula
  desc "Simple wrapper around Stencil to create files with csv file as input"
   homepage "https://github.com/callesvedin/TemplateRunner"
   url "https://github.com/callesvedin/TemplateRunner.git", tag: "0.1.1"
-  version "0.1.0"
+  version "0.1.1"
   
   depends_on :xcode => ["11.0", :build]  
 
