81d1394d04f76528a99829a8702d4b2567ce52c0
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2021-06-07 12:37:21 +0200

    Version 0.1.6 sha fix

--- Formula/template-runner.rb
+++ Formula/template-runner.rb
@@ -1,9 +1,9 @@
 class TemplateRunner < Formula
   desc "Simple wrapper around Stencil to create files with csv file as input"
   homepage "https://github.com/callesvedin/TemplateRunner"
-  url "https://github.com/callesvedin/TemplateRunner/archive/0.1.5.tar.gz"
+  url "https://github.com/callesvedin/TemplateRunner/archive/0.1.6.tar.gz"
   
- sha256 "877fbfa776f3614cd5366a299a5f7cc90ee91df73ce2bebeed9b04cecf467e36"
+ sha256 "4cff3dedb528af1d229174d08f7249c0702e9c287ccc6057cd2eb415524f1239"
 
   def install
     system "swift", "build",
